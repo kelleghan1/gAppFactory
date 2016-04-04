4010145dc135037710dc56cdf67da6bfa862643e
Anthony Simpson <ansi6622@colorado.edu>
2016-04-04 11:44:12 -0600

    added config for production enviornment for heroku db upload in knex

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,17 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    seeds: {
+      directory: './seeds'
+    }
   }
 
 };
